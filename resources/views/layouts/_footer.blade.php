767af183ae7744b36b601810bfa8a6e330064f0b
649768017 <649768017@qq.com>
2020-04-21 15:19:54 +0800

    命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,4 +1,7 @@
 <footer class="footer">
   <img src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" alt="练习" class="brand-icon">
   <a href="/">Do it better!</a>
+  <div class="float-right">
+    <a href="{{ route('about') }}">关于</a>
+  </div>
 </footer>
