00ba38ef96ab6aa3b85b200e0a027889f1b34a5a
649768017 <649768017@qq.com>
2020-04-21 15:15:11 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+  <img src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" alt="练习" class="brand-icon">
+  <a href="/">Do it better!</a>
+</footer>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,20 +3,13 @@
 <head>
   <meta charset="UTF-8">
   <title>@yield('title', 'Weibo App') - 练手项目</title>
-  <link rel="stylesheet" href="{{ mix(css/app.css) }}">
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-      <a href="/" class="navbar-brand">Weibo App<a>
-      <ul class="navbar-nav justify-content-end">
-        <li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
-      </ul>
-    </div>
-  </nav>
+  @include('layouts._header')
   <div class="container">
     @yield('content')
+    @include('layouts._footer')
   </div>
 
 </body>
