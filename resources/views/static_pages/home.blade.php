637fa8cb18bb441b91d9d72792b9d3d602ef1878
649768017 <649768017@qq.com>
2020-04-21 14:40:07 +0800

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,12 @@
 @extends('layouts.default')
 @section('content')
- <h1>主页</h1>
+ <div class="jumbotron">
+  <h1>Hello Laravel</h1>
+  <p class="lead">
+    现在所看到的是 <a href="">联手</a> 项目主页
+  </p>
+  <p>一切，将从这里开始。</p>
+  <p><a href="#" class="btn btn-lg btn-success">现在注册</a></p>
+
+ </div>
 @stop
