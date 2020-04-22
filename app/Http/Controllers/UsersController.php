e4fa83bb73c339fecbbc0d2afe9c5b8caecb7643
649768017 <649768017@qq.com>
2020-04-22 10:08:22 +0800

    登录成功后导航逻辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -31,6 +31,8 @@ class UsersController extends Controller
             'password'  =>  bcrypt($request->password),
         ]);
 
+        Auth::login($user);
+
         session()->flash('success', '欢迎，开始冲浪了^^');
         return redirect()->route('users.show', [$user]);
     }
