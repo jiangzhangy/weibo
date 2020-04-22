455b2196c58af7bf04ac4c7bcdf9a120d504ee41
649768017 <649768017@qq.com>
2020-04-22 11:52:38 +0800

    修改提示消息数组 onfo -> info

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer">
-  <img src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" alt="练习" class="brand-icon">
+  <img src="" alt="练习" class="brand-icon">
   <a href="/">Do it better!</a>
   <div class="float-right">
     <a href="{{ route('about') }}">关于</a>
